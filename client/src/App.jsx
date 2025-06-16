5ccd823fb0729ebc6ecf4f7b581f252ef3955927
ThilinaJayamal <thilinajayamal271@gmail.com>
2025-06-16 11:32:15 +0530

    protected route implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
-import Layout from './components/Layout'
+import UserLayout from './pages/UserLayout'
+import Login from './pages/Login'
 import Dashboard from './sections/Dashboard'
 import AddTransaction from './Sections/AddTransaction'
 import Trasactions from './sections/Transactions'
 import Budgets from './sections/Budgets'
 import SetBudgets from './sections/SetBudgets'
+import ProtectedRoute from './components/ProtectedRoute';
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/add-transactions' element={<AddTransaction />} />
-        <Route path='/budgets' element={<Budgets/>} />
-        <Route path='/set-budgets' element={<SetBudgets/>} />
-        <Route path='/transactions' element={<Trasactions/>} />
+
+      <Route path='login' element={<Login />} />
+
+      {/* Protected routes */}
+      <Route element={<ProtectedRoute />}>
+        <Route element={<UserLayout />}>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/add-transactions' element={<AddTransaction />} />
+          <Route path='/budgets' element={<Budgets />} />
+          <Route path='/set-budgets' element={<SetBudgets />} />
+          <Route path='/transactions' element={<Trasactions />} />
+        </Route>
       </Route>
+
     </Routes>
   )
 }
---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+// components/ProtectedRoute.jsx
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAppContext } from '../contexts/AppProvider';
+
+const ProtectedRoute = () => {
+  const { user } = useAppContext();
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
