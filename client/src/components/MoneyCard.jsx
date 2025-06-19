9f32dfe4c2ca7e0d82d0d3cc49ddd22dc81282be
ThilinaJayamal <thilinajayamal271@gmail.com>
2025-06-19 09:47:47 +0530

    NaN solved

--- client/src/components/MoneyCard.jsx
+++ client/src/components/MoneyCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function MoneyCard({ title, icon, amount, style, textColor = "",isPrice=true }) {
+function MoneyCard({ title, icon, amount=0, style, textColor = "",isPrice=true }) {
     return (
         <div className='w-full py-6 px-5 border border-gray-200 rounded-xl bg-white space-y-1'>
             <div className='flex items-center justify-between gap-4 text-sm font-semibold text-black/70'>
@@ -9,7 +9,7 @@ function MoneyCard({ title, icon, amount, style, textColor = "",isPrice=true }) 
             </div>
             {
                 isPrice ? 
-                 <span className={`text-2xl font-bold ${textColor}`}>Rs.{parseFloat(amount).toFixed(2)}</span>
+                 <span className={`text-2xl font-bold ${textColor}`}>Rs.{(parseFloat(amount)).toFixed(2)}</span>
                  :
                   <span className={`text-2xl font-bold ${textColor}`}>{parseFloat(amount).toFixed(2)}%</span>
             }
